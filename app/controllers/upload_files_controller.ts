0258ec77874c2518c0fa2e2e82e3fb283d579303
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-29 11:14:49 +0200

    Add Drive plugin from adonis instead of aws librarie debug

--- app/controllers/upload_files_controller.ts
+++ app/controllers/upload_files_controller.ts
@@ -3,29 +3,31 @@ import drive from '@adonisjs/drive/services/main'
 
 export default class UploadFilesController {
   public static async uploadFile(file: File) {
+    const disk = drive.use('spaces')
 
+    // Read the file as a buffer
+    const buffer = await file.arrayBuffer()
+    const uint8Array = new Uint8Array(buffer)
 
-    drive
-      .use('spaces')
-      .put(file.name, file.stream, {
-        visibility: 'public',
-        contentType: file.type,
-      })
-      .then(() => {
-        console.log(`File uploaded successfully: ${file.name}`)
-      })
-      .catch((error: any) => {
-        console.error(`Error uploading file: ${error.message}`)
-      })
+    // Upload the file to the specified disk
+    await disk.put(file.name, uint8Array, {
+      visibility: 'public',
+      contentType: file.type,
+    })
+
+    // Return the URL of the uploaded file
+    return disk.getUrl(file.name)
   }
 
   public static async downloadFile({ response }: HttpContext, fileName: string) {
-    const file = await drive.use('spaces').get(fileName)
+    const disk = drive.use('spaces')
+    const file = await disk.get(fileName)
     if (!file) {
       return response.notFound({ message: 'File not found' })
     }
-    response.header('Content-Disposition', `attachment; filename="${fileName}"`)
-    response.header('Content-Type', file.type)
-    response.send(file.content)
+    const fileMetaData = await disk.getMetaData(fileName)
+
+    console.log(fileMetaData)
+    response.send(file)
   }
 }
